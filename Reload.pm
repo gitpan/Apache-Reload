6db122a226684edf2c81b5823ec05939c8fa774d
Matt Sergeant <msergeant@cpan.org>
2000-08-12 16:14:44 +0000

    Import of MSERGEANT/Apache-Reload-0.02 from CPAN.
    gitpan-cpan-distribution: Apache-Reload
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MSERGEANT/Apache-Reload-0.02.tar.gz
    gitpan-cpan-author:       MSERGEANT
    gitpan-cpan-maturity:     released

--- Reload.pm
+++ Reload.pm
@@ -1,10 +1,10 @@
-# $Id: Reload.pm,v 1.3 2000/08/11 20:29:38 matt Exp $
+# $Id: Reload.pm,v 1.4 2000/08/12 16:12:28 matt Exp $
 
 package Apache::Reload;
 
 use strict;
 
-$Apache::Reload::VERSION = '0.01';
+$Apache::Reload::VERSION = '0.02';
 
 my %Stat = ($INC{"Apache/Reload.pm"} => time);
 my %INCS;
@@ -30,7 +30,14 @@ sub handler {
 	
 	my $DEBUG = ref($r) && (lc($r->dir_config("ReloadDebug") || '') eq 'on');
 	
-	while (my($key, $file) = each %INCS) {
+	if (ref($r) && (lc($r->dir_config("ReloadAll") || 'on') eq 'on')) {
+		*Apache::Reload::INCS = \%INC;
+	}
+	else {
+		*Apache::Reload::INCS = \%INCS;
+	}
+	
+	while (my($key, $file) = each %Apache::Reload::INCS) {
 		local $^W;
 #		warn "Apache::Reload: Checking mtime of $key\n" if $DEBUG;
 		
@@ -65,7 +72,8 @@ Apache::Reload - Reload this module on each request (if modified)
 
 In httpd.conf:
 
-  PerlInitHandler Apache::StatINC
+  PerlInitHandler Apache::Reload
+  PerlSetVar ReloadAll Off
 
 Then your module:
 
@@ -79,20 +87,43 @@ Then your module:
 
 =head1 DESCRIPTION
 
-This module is an adaptation of Randall Schwartz's Stonehenge::Reload
-module that attempts to be a little more intuitive and makes the usage
-easier. Like Apache::StatINC it must be installed as an Init Handler,
-but unlike StatINC it must also be used by the module you want reloading.
+This module is two things. First it is an adaptation of Randall 
+Schwartz's Stonehenge::Reload module that attempts to be a little 
+more intuitive and makes the usage easier. Stonehenge::Reload was
+written by Randall to make specific modules reload themselves when
+they changed. Unlike Apache::StatINC, Stonehenge::Reload only checked
+the change time of modules that registered themselves with 
+Stonehenge::Reload, thus reducing stat() calls. Apache::Reload also
+offers the exact same functionality as Apache::StatINC, and is thus
+designed to be a drop-in replacement. Apache::Reload only checks modules
+that register themselves with Apache::Reload if you explicitly turn off
+the StatINC emulation method (see below). Like Apache::StatINC,
+Apache::Reload must be installed as an Init Handler.
+
+To use as a StatINC replacement, simply add the following configuration
+to your httpd.conf:
+
+  PerlInitHandler Apache::Reload
+
+To only reload modules that have explicitly registered with Apache::Reload,
+add the following to the httpd.conf:
+
+  PerlInitHandler Apache::Reload
+  PerlSetVar ReloadAll Off
+  # ReloadAll defaults to On
 
 If you want to temporarily turn off reloading of a module (which is 
 slightly problematic since it won't happen until the next hit on the
-same server because of the way this thing works) you can use the 'off'
-option:
+same server because of the way this thing works, and won't start 
+reloading again until you restart the server) you can use the 'off'
+option (this only works when ReloadAll is Off):
 
   use Apache::Reload 'off';
 
 Obviously you wouldn't do that generally, but it can be useful if you 
-intend to make large changes to a particular module.
+intend to make large changes to a particular module whilst the server 
+is running, and still be able to test it compiles with perl -wc, without
+worrying about the server reloading it.
 
 =head1 AUTHOR
 
